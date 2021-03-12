9a623b1538381a0ddef6ae2874afc0f23b9977af
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-03-12 17:17:01 +0700

    Update lab22_2.cpp

--- lab22_2.cpp
+++ lab22_2.cpp
@@ -23,7 +23,28 @@ int main(){
 	loc = find(line_up.begin(),line_up.end(),"Bob");
 	line_up.insert(loc,"Oscar");
 	
-	//Write your code here
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+
+	for(int i=0;i<2;i++){
+		line_up.pop_front();
+	}
+
+	loc = find(line_up.begin(),line_up.end(),"Sanji");
+	line_up.insert(loc,"Narutu");
+
+	line_up.push_front("Prayath");
+
+	loc = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.insert(loc,"Tony");
+
+	loc = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.erase(loc);
+
+	for(int i=0;i<3;i++){
+		line_up.pop_front();
+	}
 	
 	printList(line_up);
 		
