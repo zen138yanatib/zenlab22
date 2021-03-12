4ba6f812692df1fc7ef313375c871e6b68458abe
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-03-12 17:16:42 +0700

    Update lab22_1.cpp

--- lab22_1.cpp
+++ lab22_1.cpp
@@ -63,70 +63,45 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
-#include<iostream>
-using namespace std;
-
-
-class Node{
-    public:
-    	int data;
-    	Node *next;
-    	~Node();
-};
-
-class List{
-	public:
-		Node *root;
-		int size;
-		void show();
-		void append(int);
-		void insert(int,int);
-		void remove(int);
-};
-
-Node::~Node(){
-    cout << data << " was deleted.\n";
-}
-
-void List::insert(int d,int idx){	
-	Node *n = new Node;
-	n->data = d;
-	
-	if(idx == 0){
-		n->next = root;
-		root = n;
-		return;
+void List::remove(int idx){
+	size--;
+	Node *current=root , *space;
+    if(idx != 0){
+		for(int i=0; i<idx-1; i++)current = current->next;
+			
+			space = current->next;
+			current->next=current->next->next;
+		
+	}else{
+		space = root;
+		root=root->next;
 	}
-	Node *current = root;
-	for(int i = 0; i < idx-1;i++){
-		current = current->next;
-	}
-	n->next = current->next;
-	current->next = n;		
+	delete space;
 }
 
-void List::show(){
-	Node *current = root;
-	cout << current->data << " ";	
-	while(current->next){
-		current = current->next;
-		cout << current->data << " ";
-	}	
-}
+int main(){
+	List myList = {0,0};
+	myList.append(5);
+	myList.append(7);
+	myList.append(11);
+	myList.append(4);
+	myList.append(12);
+	myList.append(45);
 
-void List::append(int d){	
-	Node *n = new Node;
-	n->data = d; n->next = NULL;
-	if(root == NULL) root = n;
-	else{
-		Node *current = root;
-		while(current->next){
-			current = current->next;
-		}
-		current->next = n;
-	}
-	size++;
-}
+	myList.insert(29,2);
+	myList.insert(33,3);
+	myList.insert(77,5);
+	myList.insert(69,0);
+	myList.show();
+	cout << "\n";
 
-//Write List::remove() here
+	myList.remove(2);
+	myList.show();
+	cout << "\n";
+	myList.remove(4);
+	myList.show();
+	cout << "\n";
+	myList.remove(0);
+	myList.show();
+	cout << "\n";
+}
